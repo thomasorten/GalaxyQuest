9cf22b662befc4578c50a461c431b00741fd2539
thomasorten <thomasorten@Thomass-MacBook-Pro.local>
2014-06-05 11:47:04 -0500

    Initial Commit

---
+++ GalaxyQuestTests/GalaxyQuestTests.m
@@ -0,0 +1,34 @@
+//
+//  GalaxyQuestTests.m
+//  GalaxyQuestTests
+//
+//  Created by Thomas Orten on 6/5/14.
+//  Copyright (c) 2014 Orten, Thomas. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GalaxyQuestTests : XCTestCase
+
+@end
+
+@implementation GalaxyQuestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
